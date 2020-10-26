d51b984a3f96256bf40e55222360f2c0a3470beb
neelkim <neelkim1989@gmail.com>
2020-10-26 22:51:08 +0500

    fix: исправить ошибку когда лайки не отрисовывались

--- src/components/App.js
+++ src/components/App.js
@@ -133,7 +133,7 @@ function App(props) {
 
   function handleCardLike(card) {
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i === currentUser._id);
 
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api
